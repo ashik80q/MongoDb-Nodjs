8c491b0b848c1039f0ff4194f8005836fbc1b9cf
Ashik khandakar <ashikbpdb@gmail.com>
2020-06-14 17:40:49 +0600

    ema-jhon backend complied

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ let client = new MongoClient(uri, { useNewUrlParser: true });
 
 const users = ['Ashik', 'Ali', 'sha', 'Tamim'];
 
-app.get('/product',(req, res)=>{
+app.get('/products',(req, res)=>{
     client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
-        const collection = client.db("onlineStore").collection("product");
-        collection.find({name:"mobile"}).limit(5).toArray((err, documents)=>{
+        const collection = client.db("onlineStore").collection("products");
+        collection.find().toArray((err, documents)=>{
     
             if(err){
                 console.log(err);
@@ -44,11 +44,59 @@ app.get('/product',(req, res)=>{
 
 
 
-app.get('/users/:id', (req, res) =>{
+app.get('/product/:key', (req, res) =>{
     
-    const id = req.params.id;
-    const name = users [id];
-    res.send({id, name});
+    const key = req.params.key;
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({key}).toArray((err, documents)=>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(documents [0]);
+            }
+            
+            
+        });
+          
+        client.close();
+      });
+})
+
+
+
+app.post('/getProductsByKey', (req, res) =>{
+    
+    const key = req.params.key;
+    const productKeys= req.body;
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({ key :{$in: productKeys}}).toArray((err, documents)=>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(documents);
+            }
+            
+            
+        });
+          
+        client.close();
+      });
 })
 //post
 app.post('/addProduct', (req, res)=>{
@@ -56,8 +104,36 @@ app.post('/addProduct', (req, res)=>{
     const product = req.body;
     client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
-        const collection = client.db("onlineStore").collection("product");
-        collection.insertOne(product,(err, result) =>{
+        const collection = client.db("onlineStore").collection("products");
+        collection.insert(product,(err, result) =>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(result.ops[0]);
+            }
+            
+        
+        })
+          
+        client.close();
+      });
+      
+})
+
+
+app.post('/placeOrder', (req, res)=>{
+    
+    const orderDetails = req.body;
+    orderDetails.orderTime = new Date();
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        collection.insertOne(orderDetails,(err, result) =>{
     
             if(err){
                 console.log(err);
