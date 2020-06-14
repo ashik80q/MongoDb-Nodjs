acd02bd251c06216618ce956d2169c731a8caac5
Ashik khandakar <ashikbpdb@gmail.com>
2020-06-14 23:29:46 +0600

    orders api added

--- index.js
+++ index.js
@@ -43,6 +43,34 @@ app.get('/products',(req, res)=>{
 });
 
 
+app.get('/orders',(req, res)=>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        collection.find().toArray((err, documents)=>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(documents);
+            }
+            
+            
+        });
+          
+        client.close();
+      });
+      
+
+});
+
+
+
+
 
 app.get('/product/:key', (req, res) =>{
     
